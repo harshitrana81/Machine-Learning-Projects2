426ff06ee2c93a703190fa0b3fd1c7ab0e23b896
harshitrana81 <ranaharshit269@gmail.com>
2026-06-03 22:06:10 +0530

    Update miniproject.py

--- miniproject.py
+++ miniproject.py
@@ -23,7 +23,7 @@ st.divider()
 col1, col2 = st.columns([1, 2], gap="large")
 
 with col1:
-    st.header("📍 User Input")
+    st.header("User Input")
     input_date = st.sidebar.date_input("Select Date", datetime.now())
     input_time = st.sidebar.time_input("Select Time", datetime.now())
     user_datetime = datetime.combine(input_date, input_time)
