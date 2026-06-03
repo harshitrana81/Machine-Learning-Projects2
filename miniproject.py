859277ed63f8e72e82f28a0db48605db0ce865f4
harshitrana81 <ranaharshit269@gmail.com>
2026-06-03 22:18:35 +0530

    Update miniproject.py

--- miniproject.py
+++ miniproject.py
@@ -24,8 +24,8 @@ col1, col2 = st.columns([1, 2], gap="large")
 
 with col1:
     st.header("User Input")
-    input_date = st.sidebar.date_input("Select Date", datetime.now())
-    input_time = st.sidebar.time_input("Select Time", datetime.now())
+    input_date = st.date_input("Select Date", datetime.now())
+    input_time = st..time_input("Select Time", datetime.now())
     user_datetime = datetime.combine(input_date, input_time)
 
     # Prediction Logic Data
